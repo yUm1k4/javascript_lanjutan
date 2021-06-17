939fb2c2ac0a3c01a5ebd21fce8c4b4394e208fb
yUm1k4 <zaiabdullah99@gmail.com>
2021-06-17 18:51:35 +0700

    Object: Object.create()

--- Object/script.js
+++ Object/script.js
@@ -1,6 +1,7 @@
 // ? Cara membuat Object pada Javascript
 
 // * 1. Object Literal
+// ! Problem: tidak efektif untuk object yang banyak
 // let mahasiswa1 = {
 //     nama: 'Zainudin',
 //     energi: 10,
@@ -20,47 +21,55 @@
 
 
 // * 2. Function Declaration
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {}
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+const methodMahasiswa =  {
+    makan: function(porsi) {
+        // this energi = this.energi + porsi
+        this.energi += porsi
+        console.log(`Hallo ${this.nama}, selamat makan!`);
+    },
+    main: function(jam) {
+        this.energi -= jam
+        console.log(`Hallo ${this.nama}, selamat bermain!`);
+    },
+    tidur: function(jam) {
+        this.energi += jam * 2
+        console.log(`Hallo ${this.nama}, selamat tidur!`);
+    }
+}
 
-//     mahasiswa.makan = function(porsi) {
+function Mahasiswa(nama, energi) {
+    // ? Object.create() sama aja seperti membuat object
+    //  bedanya kita bisa mneyimpan parameter ke parent Object (Inheritance / Warisan)
+    // jadi kita bisa mengakses method dari parent
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+
+    return mahasiswa
+}
+
+let zain = Mahasiswa('Zainudin', 10)
+let abd = Mahasiswa('Abdullah', 13)
+
+
+// * 3. Constructor Functin
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     this.makan = function(porsi) {
 //         // this energi = this.energi + porsi
 //         this.energi += porsi
 //         console.log(`Hallo ${this.nama}, selamat makan!`);
 //     }
 
-//     mahasiswa.main = function(jam) {
+//     this.main = function(jam) {
 //         this.energi -= jam
 //         console.log(`Hallo ${this.nama}, selamat bermain!`);
 //     }
-
-//     return mahasiswa
 // }
-
-// let zain = Mahasiswa('Zainudin', 10)
-// let abd = Mahasiswa('Abdullah', 13)
-
-
-// * 3. Constructor Functin
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-
-    this.makan = function(porsi) {
-        // this energi = this.energi + porsi
-        this.energi += porsi
-        console.log(`Hallo ${this.nama}, selamat makan!`);
-    }
-
-    this.main = function(jam) {
-        this.energi -= jam
-        console.log(`Hallo ${this.nama}, selamat bermain!`);
-    }
-}
-// ! cara instansiasi constructor function dengan New
-let zain = new Mahasiswa('Zainudin', 10)
+// // ! cara instansiasi constructor function dengan New
+// let zain = new Mahasiswa('Zainudin', 10)
 
 
 // * 4. Object.create()
