2a2ef0e73933672950e7ada4148b04776e69bd0d
yUm1k4 <zaiabdullah99@gmail.com>
2021-06-17 20:41:29 +0700

    Object: Prototype

--- Object/script.js
+++ Object/script.js
@@ -21,35 +21,35 @@
 
 
 // * 2. Function Declaration
-const methodMahasiswa =  {
-    makan: function(porsi) {
-        // this energi = this.energi + porsi
-        this.energi += porsi
-        console.log(`Hallo ${this.nama}, selamat makan!`);
-    },
-    main: function(jam) {
-        this.energi -= jam
-        console.log(`Hallo ${this.nama}, selamat bermain!`);
-    },
-    tidur: function(jam) {
-        this.energi += jam * 2
-        console.log(`Hallo ${this.nama}, selamat tidur!`);
-    }
-}
+// const methodMahasiswa =  {
+//     makan: function(porsi) {
+//         // this energi = this.energi + porsi
+//         this.energi += porsi
+//         console.log(`Hallo ${this.nama}, selamat makan!`);
+//     },
+//     main: function(jam) {
+//         this.energi -= jam
+//         console.log(`Hallo ${this.nama}, selamat bermain!`);
+//     },
+//     tidur: function(jam) {
+//         this.energi += jam * 2
+//         console.log(`Hallo ${this.nama}, selamat tidur!`);
+//     }
+// }
 
-function Mahasiswa(nama, energi) {
-    // ? Object.create() sama aja seperti membuat object
-    //  bedanya kita bisa mneyimpan parameter ke parent Object (Inheritance / Warisan)
-    // jadi kita bisa mengakses method dari parent
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi) {
+//     // ? Object.create() sama aja seperti membuat object
+//     //  bedanya kita bisa mneyimpan parameter ke parent Object (Inheritance / Warisan)
+//     // jadi kita bisa mengakses method dari parent
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    return mahasiswa
-}
+//     return mahasiswa
+// }
 
-let zain = Mahasiswa('Zainudin', 10)
-let abd = Mahasiswa('Abdullah', 13)
+// let zain = Mahasiswa('Zainudin', 10)
+// let abd = Mahasiswa('Abdullah', 13)
 
 
 // * 3. Constructor Functin
@@ -70,6 +70,57 @@ let abd = Mahasiswa('Abdullah', 13)
 // }
 // // ! cara instansiasi constructor function dengan New
 // let zain = new Mahasiswa('Zainudin', 10)
+
+
+// * Prototype
+// ! veri object
+// function Mahasiswa(nama, energi) {
+//     // ? secara default constructur function punya parent default namanya prototype
+//     // let this = Object.create(Mahasiswa.prototype);
+//     this.nama = nama;
+//     this.energi = energi;
+// }
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Hallo ${this.nama}, selamat makan!`
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Hallo ${this.nama}, selamat main!`
+// }
+
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energi += jam * 2;
+//     return `Hallo ${this.nama}, selamat tidur!`
+// }
+
+// let zain = new Mahasiswa('Zainudin', 10);
+
+
+// ! versi class
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan (porsi) {
+        this.energi += porsi;
+        return `Hallo ${this.nama}, selamat makan!`
+    }
+    
+    main (jam) {
+        this.energi -= jam;
+        return `Hallo ${this.nama}, selamat main!`
+    }
+    
+    tidur (jam) {
+        this.energi += jam * 2;
+        return `Hallo ${this.nama}, selamat tidur!`
+    }
+}
 
 
 // * 4. Object.create()
